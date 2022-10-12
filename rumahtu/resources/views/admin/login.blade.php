845d2eab1d049261e9f0ea3e0777274e05ab1c2d
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-10-12 10:53:19 +0700

    Update login.blade.php

--- rumahtu/resources/views/admin/login.blade.php
+++ rumahtu/resources/views/admin/login.blade.php
@@ -27,18 +27,19 @@
               <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                   <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                    <div class="card-body p-5 text-center">
-
-                      <img src="{{asset('assets/img/bps.png')}}" alt="">
+                    <div class="card-body p-5 ">
+                      <div class="text-center" >
+                        <img src="{{asset('assets/img/bps.png')}}"
+                          style="width: 185px;" alt="logo">
+                      </div>
+                      <h3 class="mb-4 text-center pt-4 font-weight-bold">Sign in</h3>
           
-                      <h3 class="mb-5">Sign in</h3>
-          
-                      <div class="form-outline mb-4">
+                      <div class="form-outline mb-4 font-weight-bold">
                         <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
                         <label class="form-label" for="typeEmailX-2">Email</label>
                       </div>
           
-                      <div class="form-outline mb-4">
+                      <div class="form-outline mb-4 font-weight-bold">
                         <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
                         <label class="form-label" for="typePasswordX-2">Password</label>
                       </div>
