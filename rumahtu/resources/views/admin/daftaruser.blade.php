bd980fee0d00f517d3e0ad094757bba7024d16ed
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-10-14 10:37:24 +0700

    fix table search and sort

--- rumahtu/resources/views/admin/daftaruser.blade.php
+++ rumahtu/resources/views/admin/daftaruser.blade.php
@@ -2,42 +2,37 @@
 @section('content')
 
 <div class="container-fluid">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Daftar Pengguna</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                  <thead>
+                                    <tr>
+                                      <th>Username</th>
+                                      <th>Email</th>
+                                      <th>Aksi</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    @foreach ($dtPengguna as $item)
+                                    <tr>
+                                      <td>{{ $item->name}}</td>
+                                      <td>{{ $item->email}}</td>
+                                      <td>
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                          <a href="{{ url('hapuspengguna', $item->id) }}" type="button" class="btn btn-danger" data-toggle="edit-atas" data-placement="right" title="Hapus data"><i class="fas fa-trash-alt"></i></a>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                    @endforeach
+                                  </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
 
-  <div class="container-fluid mt-4">
-    <div class="row">
-      <div class="col-lg-12">
-      </div>
-      <div class="col-lg-12">
-        <div class="shadow p-3 mb-4 bg-white rounded">
-          <!-- <div class="pb-3">
-            <a href="/tambahpengguna" class="btn btn-primary">Tambah data</a>
-          </div> -->
-          <table id="datapengguna" class="table table-striped table-bordered" style="width:100%">
-            <thead>
-              <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($dtPengguna as $item)
-              <tr>
-                <td>{{ $item->name}}</td>
-                <td>{{ $item->email}}</td>
-                <td>
-                  <div class="btn-group" role="group" aria-label="Basic example">
-                    <a href="{{ url('hapuspengguna', $item->id) }}" type="button" class="btn btn-danger" data-toggle="edit-atas" data-placement="right" title="Hapus data"><i class="fas fa-trash-alt"></i></a>
-                  </div>
-                </td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /#page-content-wrapper -->
-</div>
+                </div>
 @endsection
--- rumahtu/resources/views/admin/dashboard.blade.php
+++ rumahtu/resources/views/admin/dashboard.blade.php
@@ -19,6 +19,9 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+
+    
 
 </head>
 
@@ -105,6 +108,13 @@
     <script src="{{asset('assets/js/demo/chart-area-demo.js')}}"></script>
     <script src="{{asset('assets/js/demo/chart-pie-demo.js')}}"></script>
     <script src="{{asset('assets/js/demo/chart-bar-demo.js')}}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{asset('assets/vendor/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('assets/js/demo/datatables-demo.js')}}"></script>    
 
 </body>
 
