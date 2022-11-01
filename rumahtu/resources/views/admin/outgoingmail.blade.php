376c53cc5d7cd5a331c127bf6e5d174eb4aa0845
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-11-01 09:15:35 +0700

    Update outgoingmail.blade.php

--- rumahtu/resources/views/admin/outgoingmail.blade.php
+++ rumahtu/resources/views/admin/outgoingmail.blade.php
@@ -15,10 +15,10 @@
         <p>
             <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
                 aria-expanded="false" aria-controls="collapseExample">
-                Button with data-target
+                Tambah Data
             </button>
         </p>
-        <div class="collapse" id="collapseExample">
+        <div class="collapse mb-3" id="collapseExample">
             <div class="card card-body">
                 <form action="{{ route('simpansuratkeluar') }}" method="POST" enctype="multipart/form-data">
                     {{ csrf_field() }}
