e68620061d91f9c9f15edad953895161d6a93417
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-11-08 09:05:54 +0700

    Update editoutgoingmail.blade.php

--- rumahtu/resources/views/admin/editoutgoingmail.blade.php
+++ rumahtu/resources/views/admin/editoutgoingmail.blade.php
@@ -19,11 +19,11 @@
 
         <div class="col-sm-2">
             <label for="nama" class="col-form-label">NO</label>
-          <input type="number" class="form-control" id="no" placeholder="No">
+          <input type="number" required class="form-control" id="no" placeholder="No">
         </div>
         <div class="col-sm-3">
             <label for="index" class="col-form-label" >Index</label>
-            <select  id="index" name="index" class="custom-select">
+            <select required  id="index" name="index" class="custom-select">
                 <option value="{{ $dok->index}}" selected>{{ $dok->index}}</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -35,27 +35,27 @@
           </div>
           <div class="col-sm-3">
             <label for="date" class="col-form-label">Bulan</label>
-            <input type="date" class="form-control" id="date" name="date" placeholder="Bulan" value="{{ $dok->date}}">
+            <input required type="date" class="form-control" id="date" name="date" placeholder="Bulan" value="{{ $dok->date}}">
           </div>
           <div class="col-sm-4">
             <label for="kode_surat" class="col-form-label">Kode Surat</label>
-            <input type="text" class="form-control" id="kode_surat" name="kode_surat" placeholder="Kode Surat" value="{{ $dok->kode_surat}}">
+            <input required type="text" class="form-control" id="kode_surat" name="kode_surat" placeholder="Kode Surat" value="{{ $dok->kode_surat}}">
           </div>
       </div>
     <div class="form-group">
         <label for="alamat" class="col-form-label">Alamat</label>
-      <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Alamat" value="{{ $dok->alamat}}">
+      <input type="text" required class="form-control" id="alamat" name="alamat" placeholder="Alamat" value="{{ $dok->alamat}}">
     </div>
     <div class="form-group">
         <label for="tanggal" class="col-form-label">Tanggal</label>
-        <input type="date" class="form-control" id="tanggal" name="tanggal" placeholder="Perihal" value="{{ $dok->tanggal}}">
+        <input type="date" required class="form-control" id="tanggal" name="tanggal" placeholder="Perihal" value="{{ $dok->tanggal}}">
       </div>
     <div class="form-group">
         <label for="perihal" class="col-form-label">Perihal</label>
-        <input type="text-area" class="form-control" id="perihal" name="perihal" placeholder="Perihal" value="{{ $dok->perihal}}">
+        <input type="text-area" required class="form-control" id="perihal" name="perihal" placeholder="Perihal" value="{{ $dok->perihal}}">
       </div>
       <div class="form-group">
-        <input name="gambar" type="file" id="gambar" required>
+        <input name="gambar" required type="file" id="gambar" required>
       </div>
       <div class="form-group">
         {{-- <img src="{{ asset('img/'. $inf->gambar)}}" height="10%" width="50%" alt=""> --}}
